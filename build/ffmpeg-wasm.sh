068bdd040cc54f6c5990173b39bd6da64788ed26
Joel Löf <jlof@evolution.com>
2024-09-16 00:44:47 +0200

    chore: remove opt_common

--- build/ffmpeg-wasm.sh
+++ build/ffmpeg-wasm.sh
@@ -46,7 +46,7 @@ CONF_FLAGS=(
   src/fftools/ffmpeg_hw.c
   src/fftools/ffmpeg_mux.c
   src/fftools/ffmpeg_opt.c
-  src/fftools/opt_common.c
+  # src/fftools/opt_common.c
 )
 
 emcc "${CONF_FLAGS[@]}" $@
