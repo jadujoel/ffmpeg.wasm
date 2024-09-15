791cd6ad5be92393095b7bc2b2c9bc7636938d77
Joel Löf <jlof@evolution.com>
2024-09-16 01:58:44 +0200

    remove c files

--- build/ffmpeg-wasm.sh
+++ build/ffmpeg-wasm.sh
@@ -29,6 +29,7 @@ CONF_FLAGS=(
   -Wno-deprecated-declarations
   $LDFLAGS
   -sWASM_BIGINT                            # enable big int support
+  -sUSE_SDL=2                              # use emscripten SDL2 lib port
   -sMODULARIZE                             # modularized to use as a library
   ${FFMPEG_MT:+ -sINITIAL_MEMORY=1024MB}   # ALLOW_MEMORY_GROWTH is not recommended when using threads, thus we use a large initial memory
   ${FFMPEG_MT:+ -sPTHREAD_POOL_SIZE=32}    # use 32 threads
@@ -41,11 +42,11 @@ CONF_FLAGS=(
   # ffmpeg source code
   src/fftools/cmdutils.c
   src/fftools/ffmpeg.c
-  src/fftools/ffmpeg_filter.c
-  src/fftools/ffmpeg_hw.c
-  src/fftools/ffmpeg_mux.c
-  src/fftools/ffmpeg_opt.c
-  src/fftools/opt_common.c
+  # src/fftools/ffmpeg_filter.c
+  # src/fftools/ffmpeg_hw.c
+  # src/fftools/ffmpeg_mux.c
+  # src/fftools/ffmpeg_opt.c
+  # src/fftools/opt_common.c
 )
 
 emcc "${CONF_FLAGS[@]}" $@
